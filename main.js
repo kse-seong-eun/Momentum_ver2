f1c13b2b5099d822cc309d3bdefededff75bd09f
Seongeun Kim <kse3447@gmail.com>
2024-06-13 01:04:25 +0900

    FEAT: loginInput

--- main.js
+++ main.js
@@ -1,26 +1,19 @@
-import "./style/style.sass";
-// import javascriptLogo from './javascript.svg'
-// import viteLogo from '/vite.svg'
-// import { setupCounter } from './counter.js'
-
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//     <a href="https://vitejs.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-//     </a>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite logo to learn more
-//     </p>
-//   </div>
-// `
-
-// setupCounter(document.querySelector('#counter'))
+import "./style/style.scss";
 
 const LoginForm = document.querySelector("#login-form");
+const LoginInput = document.querySelector("#login-form input");
+const LoginButton = document.querySelector("#login-form button");
+const greeting = document.querySelector("#greeting");
+// string만 포함된 변수는 대문자로 쓴다.(중요한 변수가 아님)
+const HIDDEN_CLASSNAME = "hidden";
+
+function onLoginLoginSubmit(event) {
+  event.preventDefault();
+  LoginForm.classList.add(HIDDEN_CLASSNAME);
+  const userName = LoginInput.value;
+  greeting.innerText = `Hello ${userName}`;
+  // = "Hello " + userName; 이렇게 작성할 수도 있음
+  LoginForm.classList.add(HIDDEN_CLASSNAME);
+}
+
+LoginForm.addEventListener("submit", onLoginLoginSubmit);
