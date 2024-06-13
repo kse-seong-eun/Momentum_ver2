65324beeb0376a62bdbae530ece49a1b2cba3e90
Seongeun Kim <kse3447@gmail.com>
2024-06-13 17:08:17 +0900

    feat:login page with localStorage

--- main.js
+++ main.js
@@ -4,16 +4,36 @@ const LoginForm = document.querySelector("#login-form");
 const LoginInput = document.querySelector("#login-form input");
 const LoginButton = document.querySelector("#login-form button");
 const greeting = document.querySelector("#greeting");
+
 // string만 포함된 변수는 대문자로 쓴다.(중요한 변수가 아님)
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginLoginSubmit(event) {
   event.preventDefault();
   LoginForm.classList.add(HIDDEN_CLASSNAME);
   const userName = LoginInput.value;
-  greeting.innerText = `Hello ${userName}`;
-  // = "Hello " + userName; 이렇게 작성할 수도 있음
-  LoginForm.classList.add(HIDDEN_CLASSNAME);
+  localStorage.setItem(USERNAME_KEY, userName);
+  paintGreetings(userName);
 }
 
-LoginForm.addEventListener("submit", onLoginLoginSubmit);
+function paintGreetings(savedUserName) {
+  greeting.innerText = `Hello ${savedUserName}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+  // = "Hello " + userName; 이렇게 작성할 수도 있음
+}
+
+// saved 저장된값 가져오기
+const savedUserName = localStorage.getItem(USERNAME_KEY);
+// set = 저장하기
+// get = 가져오기
+// console.log(savedUserName);
+
+if (savedUserName == null) {
+  // show the form
+  LoginForm.classList.remove(HIDDEN_CLASSNAME);
+  LoginForm.addEventListener("submit", onLoginLoginSubmit);
+} else {
+  // show the greeting
+  paintGreetings(savedUserName);
+}
